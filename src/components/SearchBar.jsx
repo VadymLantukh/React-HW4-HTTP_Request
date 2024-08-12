91d248bc9d8d5c5f0261a72527cb72b07ece4964
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-08-12 09:01:04 +0300

    Add file for HW and components and fraemwork: react-icons, react-modal, modern-normalize, react-spinner, axsios

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,15 @@
+// const SearchBar = () => {
+//   <header>
+//     <form>
+//       <input
+//         type="text"
+//         autocomplete="off"
+//         autofocus
+//         placeholder="Search images and photos"
+//       />
+//       <button type="submit">Search</button>
+//     </form>
+//   </header>;
+// };
+
+// export default SearchBar;
